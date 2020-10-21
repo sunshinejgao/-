878bd86b82d86df8b57f900da9f9d0ba1d7be07f
sunshinejgao <504211994@qq.com>
2020-10-21 22:39:15 +0800

    测试1

---
+++ JavaProject/src/com/gzjg/calculator/normal/Test.java
@@ -0,0 +1,17 @@
+package com.gzjg.calculator.normal;
+
+import java.util.Scanner;//或者*
+
+public class Test {
+    public static void main(String[] args) {
+       int numberA,numberB,result;
+        Scanner scanner = new Scanner(System. in);
+        numberA = scanner.nextInt();
+        numberB = scanner.nextInt();
+        result = numberA + numberB;
+        System.out.println(result);
+
+
+
+    }
+}
---
+++ 测试/src/test.java
@@ -0,0 +1,9 @@
+/*
+ **@Author:邓少勋
+ **Created on 2020/10/21
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hahha");
+    }
+}
